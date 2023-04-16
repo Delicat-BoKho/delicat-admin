8be50125b6be6ab2629c6354f85a6dba28ad974f
Thanh Ngoc <tngocnp@pm.me>
2023-04-16 23:29:57 +0700

    Generate components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,49 @@ import { AppComponent } from './app.component';
 
 import * as $ from 'jquery';
 import 'bootstrap';
+
 import { FormsModule } from '@angular/forms';
+import { AlertComponent } from './alert/alert.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { GeneralComponent } from './general/general.component';
+import { OrdersComponent } from './order/orders/orders.component';
+import { OrderEditComponent } from './order/order-edit/order-edit.component';
+import { OrderNewComponent } from './order/order-new/order-new.component';
+import { CustomersComponent } from './customer/customers/customers.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { CustomerNewComponent } from './customer/customer-new/customer-new.component';
+import { ProductsComponent } from './product/products/products.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductNewComponent } from './product/product-new/product-new.component';
+import { ContentsComponent } from './content/contents/contents.component';
+import { ContentEditComponent } from './content/content-edit/content-edit.component';
+import { ContentNewComponent } from './content/content-new/content-new.component';
+import { SettingsComponent } from './setting/settings/settings.component';
+import { SettingPasswordComponent } from './setting/setting-password/setting-password.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    LoginComponent,
+    MenuComponent,
+    GeneralComponent,
+    OrdersComponent,
+    OrderEditComponent,
+    OrderNewComponent,
+    CustomersComponent,
+    CustomerEditComponent,
+    CustomerNewComponent,
+    ProductsComponent,
+    ProductEditComponent,
+    ProductNewComponent,
+    ContentsComponent,
+    ContentEditComponent,
+    ContentNewComponent,
+    SettingsComponent,
+    SettingPasswordComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/content/content-new/content-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-new',
+  templateUrl: './content-new.component.html',
+  styleUrls: ['./content-new.component.css']
+})
+export class ContentNewComponent {
+
+}
