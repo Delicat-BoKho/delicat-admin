176a7a462ecd164e6855c19551bea60ff90d25be
Thanh Ngoc <tngocnp@pm.me>
2023-04-17 10:20:38 +0700

    Add sidebar menu.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  faBars,
+  faXmark,
+  faChartLine,
+  faFileLines,
+  faUsers,
+  faBoxesPacking,
+  faSquareRss,
+  faGears,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-
+  faBars = faBars;
+  faCancel = faXmark;
+  faChartLine = faChartLine;
+  faOrders = faFileLines;
+  faCustomers = faUsers;
+  faProducts = faBoxesPacking;
+  faContents = faSquareRss;
+  faSettings = faGears;
+  faLogOut = faRightFromBracket;
 }
