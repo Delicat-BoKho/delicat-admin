0a4c4b688e569c8d1a143848835bf19e0f50fd69
Thanh Ngoc <tngocnp@pm.me>
2023-04-27 07:22:33 +0700

    Finish order-edit page UI.

--- src/app/order/order-edit/order-edit.component.ts
+++ src/app/order/order-edit/order-edit.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-order-edit',
   templateUrl: './order-edit.component.html',
-  styleUrls: ['./order-edit.component.css']
+  styleUrls: ['./order-edit.component.css'],
 })
 export class OrderEditComponent {
+  // Sample data
+  order: any = {
+    id: 'O003',
+    date: '2023-04-16',
+    paymentMethod: 'Credit Card',
+    products: [
+      {
+        id: 'P001',
+        name: 'Oatmeal Sharkskin Slim Fit Suit',
+        quantity: 1,
+        specifications: 'Black, M',
+      },
+      {
+        id: 'P002',
+        name: 'Product 4',
+        quantity: 2,
+        specifications: 'White, M',
+      },
+    ],
+    total: 300,
+    customerID: 'C003',
+    status: 'pending',
+    deliveryAddress: '1234 Main St, New York, NY 10001',
+  };
 
+  constructor() {}
+
+  onSave() {}
+
+  goBack() {}
 }
