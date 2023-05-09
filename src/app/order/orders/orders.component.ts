82f730b0106ff4f4034090b9d6e679f2597c0f83
Thanh Ngoc <tngocnp@pm.me>
2023-05-09 14:19:28 +0700

    Add CSS, small tweaks for HTML, add sort and search UI.

--- src/app/order/orders/orders.component.ts
+++ src/app/order/orders/orders.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { PaginationInstance } from 'ngx-pagination';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { OrderService } from 'src/app/services/order.service';
@@ -7,13 +6,23 @@ import { ProductService } from 'src/app/services/product.service';
 import { Router, RouterLink } from '@angular/router';
 import { Order } from 'src/app/models/order';
 import { AuthService } from 'src/app/services/auth.service';
+import {
+  faSearch,
+  faSort,
+  faSortDesc,
+  faSortAsc,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-  modalRef: BsModalRef | null = null;
+  faSearch = faSearch;
+  faSort = faSort;
+  faSortDesc = faSortDesc;
+  faSortAsc = faSortAsc;
+
   orders: any;
   errMessage: string = '';
   orderToDelete: any;
@@ -21,7 +30,7 @@ export class OrdersComponent implements OnInit {
   public productId: string = '';
   public orderId: string = '';
   orderIdDelete: string = '';
-  @ViewChild('deleteConfirmationModal') deleteConfirmationModal: any;
+
   ngOnInit(): void {
     this.authService.checkValidUser();
     // Code to view all orders here
@@ -55,7 +64,6 @@ export class OrdersComponent implements OnInit {
   }
 
   constructor(
-    private modalService: BsModalService,
     private service: OrderService,
     private authService: AuthService,
     private Pservice: ProductService,
@@ -65,31 +73,25 @@ export class OrdersComponent implements OnInit {
     this.orderId = id;
     console.log('Giá trị của orderID:', this.orderId);
   }
-  confirmDeleteOrder(order: any): void {
-    this.orderIdDelete = order.id;
-    this.modalRef = this.modalService.show(this.deleteConfirmationModal, {
-      class: 'modal-dialog-centered',
-    });
-  }
-
-  deleteOrder() {
-    this.service.deleteOrder(this.orderIdDelete);
-    if (this.modalRef) {
-      this.modalRef.hide();
-    }
-  }
-
-  cancelDeleteOrder() {
-    this.orderToDelete = null;
-    if (this.modalRef) {
-      this.modalRef.hide();
-    }
-  }
 
   ViewOrderDetail(id: string) {
     this.router.navigate(['order-edit/' + id]);
   }
   ViewCustomerDetail(id: string) {
     this.router.navigate(['customer-edit/' + id]);
+  }
+
+  currentSortState: string = 'default';
+  sortASC() {
+    this.currentSortState = 'asc';
+    // WRITE CODE HERE
+  }
+  sortDESC() {
+    this.currentSortState = 'desc';
+    // WRITE CODE HERE
+  }
+  sortDefault() {
+    this.currentSortState = 'default';
+    // WRITE CODE HERE
   }
 }
