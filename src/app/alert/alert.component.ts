68ba520b837aae388dd9ad2735636b4baf134109
Thanh Ngoc <tngocnp@pm.me>
2023-05-10 23:02:07 +0700

    Chart0: Sales by product types.

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import * as echarts from 'echarts';
+import { OrderService } from '../services/order.service';
+import { Order } from '../models/order';
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
@@ -9,12 +11,106 @@ import * as echarts from 'echarts';
 })
 export class AlertComponent implements OnInit {
   products: Product[] = [];
+  orders: Order[] = [];
   errMessage: string = '';
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private orderService: OrderService
+  ) {}
 
   ngOnInit(): void {
+    this.renderChart0();
     this.renderChart1();
+  }
+
+  renderChart0(): void {
+    this.orderService.getOrders().subscribe({
+      next: (res: any) => {
+        this.orders = res;
+        this.productService.getProducts().subscribe({
+          next: (res: any) => {
+            this.products = res;
+            // Create a map to store the total sales for each product type
+            const salesByType = new Map<string, number>();
+            this.products.forEach((product) => {
+              if (product.type) {
+                salesByType.set(product.type, 0);
+              }
+            });
+            this.orders.forEach((order) => {
+              order.saleProducts.forEach((saleProduct) => {
+                const product = this.products.find(
+                  (p) => p.id === saleProduct.productId
+                );
+                if (product && salesByType.has(product.type)) {
+                  const type = product.type;
+                  const quantity = saleProduct.quantity;
+                  const totalSales = salesByType.get(type) || 0;
+                  salesByType.set(type, totalSales + quantity);
+                }
+              });
+            });
+
+            const chartData = Array.from(salesByType).map(([type, sales]) => ({
+              name: type,
+              value: sales,
+            }));
+
+            let chartElement = document.getElementById('chart0');
+
+            if (chartElement) {
+              let chart = echarts.init(chartElement);
+
+              let option = {
+                textStyle: {
+                  fontFamily: "'Kanit', sans-serif",
+                },
+                title: {
+                  text: 'Sales by Product Type',
+                  left: 'center',
+                },
+                tooltip: {
+                  trigger: 'item',
+                  formatter: '{a} <br/>{b}: {c} ({d}%)',
+                },
+                series: [
+                  {
+                    name: 'Sales',
+                    type: 'pie',
+                    radius: ['40%', '70%'],
+                    avoidLabelOverlap: false,
+                    itemStyle: {
+                      borderRadius: 10,
+                      borderColor: '#fff',
+                      borderWidth: 2,
+                    },
+                    data: chartData,
+                    label: {
+                      show: true,
+                      formatter: '{b} ({d}%)',
+                    },
+                    labelLine: {
+                      show: true,
+                    },
+                  },
+                ],
+              };
+
+              chart.setOption(option);
+            }
+          },
+          error: (err) => {
+            this.errMessage = err;
+            console.log('Error get products: ' + err);
+          },
+        });
+      },
+      error: (err) => {
+        this.errMessage = err;
+        console.log('Error inside get orders: ' + err);
+      },
+    });
   }
 
   renderChart1(): void {
