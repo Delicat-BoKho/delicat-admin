087361d0c01d8debe5ee076b1226038306e7ff38
dacsang <mandacsang16122002@gmail.com>
2023-05-10 21:44:54 +0700

    add checkvalid user in homePage

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.css']
+  styleUrls: ['./general.component.css'],
 })
 export class GeneralComponent {
-
+  constructor(private authService: AuthService) {
+    this.authService.checkValidUser();
+  }
 }
