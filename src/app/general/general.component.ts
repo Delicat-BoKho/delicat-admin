26de05903e7eb38eb73c15fa80f5169d33b71a59
dacsang <mandacsang16122002@gmail.com>
2023-05-10 15:02:07 +0700

    test chart

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import * as echarts from 'echarts';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
-  styleUrls: ['./general.component.css']
+  styleUrls: ['./general.component.css'],
 })
 export class GeneralComponent {
+  graph: any;
+  option: any;
+  chartOption!: EChartsOption;
+  errMessage: string = '';
+  chart() {
+    type EChartsOption = echarts.EChartsOption;
+    var chartDom = document.getElementById('main')!;
+    var myChart = echarts.init(chartDom);
+    var option: EChartsOption;
 
+    option = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          areaStyle: {},
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }
 }
